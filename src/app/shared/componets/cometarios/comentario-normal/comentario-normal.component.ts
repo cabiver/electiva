168b21b6dd47b4fb4c4ced714316b8a97c46c161
carlos vergel01 <carlosvergel01@gmail.com>
2021-10-26 19:44:33 -0500

    base

---
+++ src/app/shared/componets/cometarios/comentario-normal/comentario-normal.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-comentario-normal',
+  templateUrl: './comentario-normal.component.html',
+  styleUrls: ['./comentario-normal.component.css']
+})
+export class ComentarioNormalComponent implements OnInit {
+  @Input() coment: String ="";
+  @Input() fecha: String ="";
+  @Input() name: String ="";
+  @Input() image: String="assets/Img/comentario3.gif";
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,13 +3,17 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './componets/header/header.component';
 import { FooterComponent } from './componets/footer/footer.component';
 import { RouterModule } from '@angular/router';
+import { ComentarioNormalComponent } from './componets/cometarios/comentario-normal/comentario-normal.component';
+import { ComentarioJokerComponent } from './componets/cometarios/comentario-joker/comentario-joker.component';
 
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ComentarioNormalComponent,
+    ComentarioJokerComponent
   ],
   imports: [
     RouterModule,
