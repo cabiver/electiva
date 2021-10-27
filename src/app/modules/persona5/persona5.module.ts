23220db82408b2426f84ad489ddcb4426250f7c3
carlos vergel <carlosvergel01@gmail.com>
2021-10-27 11:28:56 -0500

    persona

--- src/app/modules/persona5/persona5.module.ts
+++ src/app/modules/persona5/persona5.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { Persona5RoutingModule } from './persona5-routing.module';
 import { Persona5PageComponent } from './pages/persona5-page/persona5-page.component';
+import { HeaderPersonaComponent } from './pages/header-persona/header-persona.component';
 
 
 @NgModule({
   declarations: [
-    Persona5PageComponent
+    Persona5PageComponent,
+    HeaderPersonaComponent
   ],
   imports: [
     CommonModule,
